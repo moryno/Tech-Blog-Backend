e31ac53fe51aabdb6d5ce9d2ae15b820566916a7
Maurice <mauricenganga41@gmail.com>
2022-07-29 19:33:11 +0300

    Modify port

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen(process.env.PORT || 3030, () =>
+app.listen(process.env.PORT || 3000, () =>
   console.log("Server running at port 3030")
 );
