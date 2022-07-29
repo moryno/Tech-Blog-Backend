7b817085ba4af1496d9821785a96fdca704d1257
Maurice <mauricenganga41@gmail.com>
2022-07-29 18:18:58 +0300

    First commit

---
+++ routes/categories.js
@@ -0,0 +1,29 @@
+// eshint version:6
+
+const router = require("express").Router();
+const Category = require("../models/Category");
+
+router.post("/", async(req,res)=>{
+    const newCategory = new Category(req.body);
+    try{ 
+       const category = await newCategory.save();
+       res.status(200).json(category);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+router.get("/", async(req,res)=>{
+    
+    try{ 
+       const cats = await Category.find();
+       res.status(200).json(cats);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
+module.exports = router; 
