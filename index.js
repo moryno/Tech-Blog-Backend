002dedf2c3f05b315d791b55fd176cc64b3f07a9
Maurice <mauricenganga41@gmail.com>
2022-07-29 19:20:57 +0300

    Add port for deployment

--- index.js
+++ index.js
@@ -11,32 +11,30 @@ const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 dotenv.config();
 
-
-
-mongoose.connect(process.env.MONGO_URL)
-.then(console.log("Connected to mongoDB."))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to mongoDB."))
+  .catch((err) => console.log(err));
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "images");
-    },
-    filename:(req, file, cb)=>{
-        cb(null, req.body.name);
-    }
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
 });
 
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 
-app.post("/api/upload", upload.single("file"), (req,res)=>{
-    res.status(200).json("File has been uploaded");
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
 });
 
 app.use("/api/auth", authRoute);
@@ -44,12 +42,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-
-
-
-
-
-
-
-
-app.listen(3030, ()=> console.log("Server running at port 3030"));
+app.listen(process.env.PORT || 3030, () =>
+  console.log("Server running at port 3030")
+);
